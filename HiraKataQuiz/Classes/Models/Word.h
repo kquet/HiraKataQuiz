4f8a10cfb4f02cef58eb88d13aea484ad29c8715
kquet <quet.kael@gmail.com>
2014-03-25 22:14:22 -0400

    JSON Update: - Added Words.json - Corrected Symbols.json to valid JSON

---
+++ HiraKataQuiz/Classes/Models/Word.h
@@ -0,0 +1,13 @@
+//
+//  Word.h
+//  HiraKataQuiz
+//
+//  Created by Kael Quet on 2014-03-25.
+//  Copyright (c) 2014 Kael Quet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Word : NSObject
+
+@end
---
+++ HiraKataQuiz/Classes/Models/Word.m
@@ -0,0 +1,13 @@
+//
+//  Word.m
+//  HiraKataQuiz
+//
+//  Created by Kael Quet on 2014-03-25.
+//  Copyright (c) 2014 Kael Quet. All rights reserved.
+//
+
+#import "Word.h"
+
+@implementation Word
+
+@end
